f47d860d926a6ff269ce9d6e80c31d934ddec251
Miguel Gomez <mgomezmendoza@gmail.com>
2017-09-22 17:26:14 -0500

    Archivos Actualizados

--- models/todo_model.py
+++ models/todo_model.py
@@ -2,6 +2,6 @@ from odoo import models, fields
 class TodoTask(models.Model):
 	_name = 'todo.task'
 	_description = 'Tareas Pendientes'
-	name = fields.Char('Descripcion', required = True)
+	name = fields.Char('Descripcion', required=True)
 	is_done = fields.Boolean('Hecho?')
 	active = fields.Boolean('Activo?', default=True)
